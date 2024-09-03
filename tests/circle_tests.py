3e03b3b443932aebbbbac01e05c7d33167e7b219
Tatiana Barga <tatibspersonal@gmail.com>
2024-09-03 11:20:24 -0500

    circle create test passes

--- tests/circle_tests.py
+++ tests/circle_tests.py
@@ -7,3 +7,15 @@ class CircleTests(APITestCase):
     def setUp(self):
         self.user = User.objects.create(name="Test User", uid="12345")
         self.circle = Circle.objects.create(creator=self.user, name="Test Circle")
+
+    def test_create_circle(self):
+        url = '/circles'
+        data = {
+            "creator": self.user.id,
+            "name": "New Circle"
+        }
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Circle.objects.count(), 2)
+        self.assertEqual(Circle.objects.get(id=response.data['id']).name, "New Circle")
