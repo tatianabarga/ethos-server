2caefdf91eaa5da9bccf068f45c2b73b4d468648
Tatiana Barga <tatibspersonal@gmail.com>
2024-09-03 10:29:21 -0500

    log tests setup passes

---
+++ tests/log_tests.py
@@ -0,0 +1,19 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+from ethosapi.models import User, Profile, Log
+from django.urls import reverse
+from datetime import date
+
+class LogTests(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(name="Test User", uid="12345")
+        self.profile = Profile.objects.create(creator=self.user, bio="Test Bio", name="Test Name")
+        self.log = Log.objects.create(
+            creator=self.user,
+            profile=self.profile,
+            score_impact="Positive",
+            title="Test Title",
+            description="Test Description",
+            event_date=date.today()
+        )
